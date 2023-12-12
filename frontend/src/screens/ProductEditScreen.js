d41b487b80d0633be93ccc90b40e73bdb9831935
joumaamhamad <mhmdjom198@gmail.com>
2023-12-12 12:31:30 +0200

    e-commerce-34-Implement-Update-Product

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -8,6 +8,7 @@ import { Form , Button , Container } from 'react-bootstrap';
 import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { toast } from 'react-toastify';
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -21,6 +22,15 @@ const reducer = (state, action) => {
         case 'FETCH_FAIL':
             return { ...state, loading: false, error: action.payload };
 
+        case 'UPDATE_REQUEST':
+            return { ...state, loadingUpdate: true };
+
+        case 'UPDATE_SUCCESS':
+            return { ...state, loadingUpdate: false };
+
+        case 'UPDATE_FAIL':
+            return { ...state, loadingUpdate: false };
+
         default:
             return state;
     }
@@ -28,14 +38,14 @@ const reducer = (state, action) => {
 
 export default function ProductEditScreen() {
 
+    const navigate = useNavigate();
     const params = useParams();
     const { id: productId } = params;
 
     const { state } = useContext(Store);
     const { userInfo } = state;
 
-    const [{ loading, error }, dispatch] = useReducer(reducer, {
-
+    const [{ loading, error, loadingUpdate }, dispatch] = useReducer(reducer, {
         loading: true,
         error: '',
     });
@@ -73,6 +83,37 @@ export default function ProductEditScreen() {
         fetchData();
     }, [productId]);
 
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            dispatch({ type: 'UPDATE_REQUEST' });
+            await axios.put(`/api/products/${productId}`,
+                {
+                    _id: productId,
+                    name,
+                    slug,
+                    price,
+                    image,
+                    category,
+                    brand,
+                    countInStock,
+                    description,
+                },
+                {
+                    headers: { Authorization: `Bearer ${userInfo.token}` },
+                }
+                );
+
+            dispatch({type: 'UPDATE_SUCCESS',});
+            toast.success('Product updated successfully');
+            navigate('/admin/products');
+
+        } catch (err) {
+            toast.error(getError(err));
+            dispatch({ type: 'UPDATE_FAIL' });
+        }
+    };
+
 return (
     
     <Container className="small-container">
@@ -86,7 +127,7 @@ return (
         ) : error ? (
             <MessageBox variant="danger">{error}</MessageBox>
         ) : (
-            <Form>
+            <Form onSubmit={submitHandler}>
                 <Form.Group className="mb-3" controlId="name">
                     <Form.Label>Name</Form.Label>
                     <Form.Control value={name} onChange={(e) => setName(e.target.value)} required/>
@@ -135,7 +176,10 @@ return (
 
 
                 <div className="mb-3">
-                    <Button type="submit">Update</Button>
+                    <Button disabled={loadingUpdate} type="submit">
+                        Update
+                    </Button>
+                    {loadingUpdate && <LoadingBox></LoadingBox>}
                 </div>
         </Form>
     )}
