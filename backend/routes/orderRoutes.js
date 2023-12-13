8f8216fca24d649f312819bff1eb372e41328076
joumaamhamad <mhmdjom198@gmail.com>
2023-12-13 11:41:56 +0200

    e-commerce-36-List-Orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -9,6 +9,12 @@ import { isAdmin } from '../utils.js';
 
 const orderRouter = express.Router();
 
+orderRouter.get('/', isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
+    
+    const orders = await Order.find().populate('user', 'name');
+    res.send(orders);
+}));
+
 orderRouter.post('/' ,isAuth ,expressAsyncHandler( async (req , res) => {
     
     const newOrder = new Order({
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,6 +28,7 @@ import DashboardScreen from './screens/DashboardScreen';
 import AdminRoute from './components/AdminRoute';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
 
 
 function App() {
@@ -190,6 +191,15 @@ function App() {
                 element={
                   <AdminRoute>
                     <DashboardScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+
+              <Route
+                path="/admin/orders"
+                element={
+                  <AdminRoute>
+                    <OrderListScreen />
                   </AdminRoute>
                 }
               ></Route>
